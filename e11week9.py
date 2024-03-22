465adb283e769a9042c20b4a3f698df42eb1ab27
emcha27 <157768214+emcha27@users.noreply.github.com>
2024-03-22 14:51:52 -0700

    Update e11week9.py

--- e11week9.py
+++ e11week9.py
@@ -1,19 +1,23 @@
 # setup an event trigger for detecting a pulse using the GPIO pin on the raspberry pi:
+import sys
+import csv
 import time
 import RPi.GPIO as GPIO
 
+start_time = time.time()
+run_time = int(sys.argv[1])
+interval = int(sys.argv[2])
+filename = sys.argv[3]
+file = open(filename,'w',newline='')
+writer = csv.writer(file)
+
+df = ["Time","Counts"]
+writer.writerow(df)
 pulse_count = 0
 
 # Setup 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# # In case the channel is not connected 
-# channel = GPIO.wait_for_edge(17, GPIO.FALLING)
-# if channel is None:
-#     print('Timeout occurred')
-# else:
-#     print('Edge detected on channel', channel)
 
 # Callback function to increment count
 def pulse_detected(self):
@@ -24,11 +28,14 @@ def pulse_detected(self):
 # Add event detect for counting pulses
 GPIO.add_event_detect(17, GPIO.FALLING, callback=pulse_detected)
 
-# Count number of pulses in the last min 
+# Count number of pulses in the last interval
 try:
-    while True:
+    while (now-start_time) < run_time:
+        now = time.time()
+        time.sleep(interval)
+        print(f'Pulses detected in the last {interval} sec: {pulse_count}')
+        data = [now,pulse_count]
+        writer.writerow(data)
         pulse_count = 0
-        time.sleep(60)
-        print(f'Pulses detected in the last minute: {pulse_count}')
 finally:
     GPIO.cleanup()
